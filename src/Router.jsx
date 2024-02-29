4d9111445f091312b20a9a7be44778fbafc42294
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-02-29 16:05:45 +0100

    Trying to fix vercel issue with changing pages n°1

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,11 +6,16 @@ import Projects from "./pages/Projects"
 import './styles/main.scss'
 
 function App() {
+  // Dynamically set the basename based on the current location pathname, because process doesn't work
+  const basename = window.location.pathname.startsWith('/your-deployment-subpath')
+    ? '/your-deployment-subpath'
+    : '/';
+
   return (
-    <Router>
+    <Router basename={basename}>
         <Routes>
           <Route path='/' element={<Landing />} />
-          <Route path='/projects' element={<Projects />} />
+          <Route path='projects' element={<Projects />} />
         </Routes>
       </Router>
   );
